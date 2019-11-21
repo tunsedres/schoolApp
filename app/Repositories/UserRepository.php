acc474fe960175be2cdf47fae760126b71edba26
Tuncay Elvanağaç <tunsedres@gmail.com>
2019-11-21 13:32:19 +0300

    repositories added

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace App\Repositories;
+
+
+class UserRepository implements UserRepositoryInterface
+{
+
+    /**
+     * Get's a post by it's ID
+     *
+     * @param int
+     */
+    public function get($post_id)
+    {
+        // TODO: Implement get() method.
+    }
+
+    /**
+     * Get's all posts.
+     *
+     * @return mixed
+     */
+    public function all()
+    {
+        // TODO: Implement all() method.
+    }
+
+    /**
+     * Deletes a post.
+     *
+     * @param int
+     */
+    public function delete($post_id)
+    {
+        // TODO: Implement delete() method.
+    }
+
+    /**
+     * Updates a post.
+     *
+     * @param int
+     * @param array
+     */
+    public function update($post_id, array $post_data)
+    {
+        // TODO: Implement update() method.
+    }
+}
---
+++ app/Repositories/UserRepositoryInterface.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Repositories;
+
+
+interface UserRepositoryInterface
+{
+    /**
+     * Get's a post by it's ID
+     *
+     * @param int
+     */
+    public function get($post_id);
+
+    /**
+     * Get's all posts.
+     *
+     * @return mixed
+     */
+    public function all();
+
+    /**
+     * Deletes a post.
+     *
+     * @param int
+     */
+    public function delete($post_id);
+
+    /**
+     * Updates a post.
+     *
+     * @param int
+     * @param array
+     */
+    public function update($post_id, array $post_data);
+
+}
